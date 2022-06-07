4181fa5e8c3d793d62cb9a65c0d50e78f4930d3b
“FalkenDev” <kafa21@student.bth.se>
2022-06-07 15:42:31 +0100

    Fixed tempory login page

--- App.tsx
+++ App.tsx
@@ -7,60 +7,116 @@ import { Base } from './styles/index.js';
 import { useEffect, useState } from 'react';
 import FlashMessage from 'react-native-flash-message';
 import Home from './components/Home';
+import LoginScreen from "react-native-login-screen";
+import { ImageBackground, Text, View } from 'react-native';
+import AuthModel from "./models/auth";
+import { showMessage } from 'react-native-flash-message';
 
 const Tab = createBottomTabNavigator();
 const routeIcons = {
   "Hem": "home",
-  "Alla stationer":"transfer-within-a-station",
-  "Trafikinfo":"train",
-  "Karta":"map",
-  "Logga in":"login",
+  "Projects": "folder-open",
+  "Inspiration": "lightbulb-outline",
+  "To-Do": "auto-awesome-motion",
+  "Planner": "calendar-today",
+  "Links": "link",
 };
 
 export default function App() {
   console.log("------| app.tsx |------");
+  const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(false);
+  let setEmail = "";
+  let setPassword = "";
+  //console.log(auth);
+  console.log(setEmail);
+  console.log(setPassword);
 
-  return (
-    <SafeAreaView style={Base.container}>
-      <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
-        headerStyle: {
-          backgroundColor: '#1a1a1a',
-        },
-        headerTitleStyle: {
-          color: 'white'
-        },
-        tabBarStyle: {
-          backgroundColor: '#1a1a1a',
-          height: 60,
-          borderTopWidth: 0,
-          paddingHorizontal: 5,
-          paddingBottom: 5,
-        },
-        tabBarLabelStyle: {
-          fontSize: 12,
-          marginTop: -5,
-          paddingBottom: 4,
-        },
-        tabBarIcon: ({ color, size }) => {
-          let iconName = routeIcons[route.name] || "warning";
+  async function doLogin() {
+    if (setPassword && setEmail) {
+        const result = await AuthModel.login(setEmail, setPassword);
+        setEmail = "";
+        setPassword = "";
 
-          return <MaterialIcons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: 'white',
-        tabBarInactiveTintColor: 'gray',
-      })}
-    >
-          <Tab.Screen name="Hem" component={Home}/>
-          <Tab.Screen name="Projects" component={Home}/>
-          <Tab.Screen name="Inspiration" component={Home}/>
-          <Tab.Screen name="To-Do" component={Home}/>
-          <Tab.Screen name="Planner" component={Home}/>
-          <Tab.Screen name="Links" component={Home}/>
-        </Tab.Navigator>
-      </NavigationContainer>
-      <StatusBar style="light" />
-      <FlashMessage position="top"/>
-    </SafeAreaView>
-  );
+        if(result.type === "success") {
+            setIsLoggedIn(true);
+        }
+        showMessage(result);
+    } else {
+        showMessage({
+            message: "E-post eller lössenord fattas",
+            description: "Var vänligen att skriva in både e-post och lössenord",
+            type: "warning"
+        });
+    }
+}
+  let statement1 = false;
+  let statement2 = false;
+  if (isLoggedIn === false) {
+    return (
+      <SafeAreaView style={Base.container}>
+          <LoginScreen
+            logoImageSource={require("./assets/FalkenDevLogo.png")}
+            onLoginPress={() => {
+              doLogin()
+            }}
+            onSignupPress={() => {}}
+            onEmailChange={(emailString: string) => {
+              setEmail = emailString;
+            }}
+            onPasswordChange={(passwordString: string) => {
+              setPassword = passwordString;
+            }}
+            disableSocialButtons={false}
+            disableSignup={true} // Ingen signup atm
+          >
+          </LoginScreen>
+        <StatusBar style="light" />
+        <FlashMessage position="top"/>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={Base.container}>
+        <NavigationContainer>
+        <Tab.Navigator screenOptions={({ route }) => ({
+          headerStyle: {
+            backgroundColor: '#1a1a1a',
+          },
+          headerTitleStyle: {
+            color: 'white'
+          },
+          tabBarStyle: {
+            backgroundColor: '#1a1a1a',
+            height: 60,
+            borderTopWidth: 0,
+            paddingHorizontal: 5,
+            paddingBottom: 5,
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+            marginTop: -5,
+            paddingBottom: 4,
+          },
+          tabBarIcon: ({ color, size }) => {
+            let iconName = routeIcons[route.name] || "warning";
+
+            return <MaterialIcons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+            <Tab.Screen name="Hem" component={Home}/>
+            <Tab.Screen name="Projects" component={Home}/>
+            <Tab.Screen name="Inspiration" component={Home}/>
+            <Tab.Screen name="To-Do" component={Home}/>
+            <Tab.Screen name="Planner" component={Home}/>
+            <Tab.Screen name="Links" component={Home}/>
+          </Tab.Navigator>
+        </NavigationContainer>
+        <StatusBar style="light" />
+        <FlashMessage position="top"/>
+      </SafeAreaView>
+    );
+  }
 }
